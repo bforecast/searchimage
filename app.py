e7d0ce6971cfc735d2a518fa276a52789602c8b2
bforecast <brilliantforecast@gmail.com>
2024-06-24 14:07:26 -0400

    add some comments

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pathlib
 from PIL import Image
 
 import os
@@ -7,7 +6,6 @@ import io
 from dotenv import load_dotenv
 # from extract_thinker.llm import LLM
 # from extract_thinker import DocumentLoaderTesseract
-
 # from extract_thinker.extractor import Extractor
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
@@ -19,24 +17,19 @@ load_dotenv()
 cwd = os.getcwd()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
+#extract text from image and return the data focused in type: dict
 def extract_with_gemini(img):
     model = genai.GenerativeModel('gemini-1.5-flash',
                 generation_config={"response_mime_type": "application/json"})
-
-    response = model.generate_content(["find the product number, issue date. Turn issue date to the date format.Return as the json format.", img],
+    prompt = "find the product number, issue date. Turn issue date to the date format.Return as the json format."
+    response = model.generate_content([prompt, img],
                   safety_settings={
-                            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-                            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
                             HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
                         },
                   stream=True)
     response.resolve()
     result_dict = json.loads(response.candidates[0].content.parts[0].text)
     return result_dict
-
-
-
-import xml.etree.ElementTree as ET
     
 # Streamlit app
 def main():
@@ -46,23 +39,20 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png", "pdf"])
 
     if uploaded_file is not None:
-            # 获取文件类型
+            # get file type
             file_type = uploaded_file.type
             st.write(file_type)
-            # Load and display the image
-            # 显示图片
+            # get image from different file formats
             if file_type in ["image/jpeg", "image/png"]:
                 pil_image = Image.open(uploaded_file)
 
-            # 显示PDF文件并提取第一页图片
             elif file_type == "application/pdf":
-                # 打开PDF文件
                 doc = fitz.open(uploaded_file)
                 for page in doc:  # iterate through the pages
                     pixmap = page.get_pixmap()  # render page to an image
                     pil_image =  Image.frombytes("RGB", [pixmap.width, pixmap.height], pixmap.samples)
                 doc.close()
-
+            # show the image
             st.image(pil_image, caption=f'Uploaded {file_type}.', use_column_width=True)
 
             # Generate UI description
@@ -70,7 +60,6 @@ def main():
                 st.write("🧑‍💻 Looking at your Images...")
                 result = extract_with_gemini(pil_image)
                 st.write(result)
-
 
 if __name__ == "__main__":
     main()
